feca340e8810f22bdbb3490e1a78c20b61bae844
IlliterateElf <illiterate.elf@outlook.com>
2022-08-14 17:59:36 -0700

    made all pages, updating NavBar, routing
    All pages that are planned currently now have a .jsx file associated with them that just says their tab name.
    
    NavBar now has all of these tabs in its nav-items.
    
    Routing now works inside of the NavBar and goes to all of the other pages of the website.

--- my-favorite-movies/src/NavBar.jsx
+++ my-favorite-movies/src/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
     render() {
@@ -6,17 +7,26 @@ export default class NavBar extends Component {
             <React.Fragment>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
+                        <Link to="/" className="navbar-brand">My Favorite Movies</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    <Link to="/" className="nav-link">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
+                                    <Link to="/myfavorites" className="nav-link">My Favorites</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/explore" className="nav-link">Explore</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/account" className="nav-link">Account</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link">Login</Link>
                                 </li>
                             </ul>
                             <form className="d-flex" role="search">
